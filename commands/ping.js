2e6d95d294242c9793e44021fb141cf91a60eb06
Shadwow <>
2022-11-26 18:55:17 +0000

    Initial commit

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder} = require('discord.js')
+
+module.exports = {
+	help: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Tester si le bot fonctionne.'),
+	async execute(interaction) {
+		await interaction.reply('Le bot est actuellement en marche ! En cas de problème avec une commande, nous vous invitons à le signaler au staff.');
+	},
+};
